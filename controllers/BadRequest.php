14e2065818ab805381c51e69418f82a32da661cd
Ahmad Gneady <support@bigprof.com>
2018-03-22 10:41:45 +0200

    Implemented route() * Handling of non-existing classes in auto loader * Added BadRequest controller.

---
+++ controllers/BadRequest.php
@@ -0,0 +1,6 @@
+<?php
+	class BadRequest {
+		public function not_found(){
+			echo 'Page not found';
+		}
+	}
--- loader.php
+++ loader.php
@@ -1,9 +1,21 @@
 <?php
 	spl_autoload_register(function($class_name){
-		include("./controllers/{$class_name}.php");
+		$include = "./controllers/{$class_name}.php";
+		if(is_file($include)){
+			include($include);
+		}
 	});
 	
 	function route($controller, $action) {
+		if(!class_exists($controller) || !is_callable([$controller, $action])){
+			$controller = 'BadRequest';
+			$action = 'not_found';
+		}
+
+		/*
 		$c = new $controller();
 		$c->$action();
+		*/
+		$c = new $controller;
+		call_user_func_array([$c, $action], []);
 	}
